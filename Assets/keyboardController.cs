145be2822cd0d0d41f1bdd9767da804e6b21dfcf
LukeSkypewalker <work37@gmail.com>
2017-02-26 00:27:44 +0300

    test scene
    and first pivot attempt (simple one)

---
+++ Assets/keyboardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboardController : MonoBehaviour
+{
+    public Transform bottom;
+    private Transform center;
+    public float k=1;
+	// Use this for initialization
+	void Start ()
+	{
+	    center = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float alpha = Input.GetAxis("Horizontal");
+        center.Rotate(0, 0, alpha);
+        bottom.Rotate(0, 0, k*-alpha);
+
+	}
+}
